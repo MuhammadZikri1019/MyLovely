dfd7230db25f081af8ab1db2c5911c812fdc3da5
MuhammadZikri1019 <muhammadzikrialfadani01@gmail.com>
2026-04-23 19:22:54 +0700

    Update koneksi.php: Fix database name untuk Wasmer

--- koneksi.php
+++ koneksi.php
@@ -1,14 +1,15 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "my_lovely";
+$host = "db.fr-pari1.bengt.wasmernet.com";
+$port = 10272;
+$user = "a0ca7d1578e58000f353bcfc84ab";
+$pass = "069ea0ca-7d15-79e7-8000-396c4103dd3c";
+$db   = "dboSUaE6DmNSaLj6JUVYBthp";
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 if ($conn->connect_error) {
-    die("<h3>❌ Koneksi Database Gagal!</h3><p>Error: " . $conn->connect_error . "</p><p>Silakan jalankan: <a href='setup.php'>http://localhost/MyLovely/setup.php</a></p>");
+    die("<h3>❌ Koneksi Database Gagal!</h3><p>Error: " . $conn->connect_error . "</p><p>Silakan cek kembali kredensial Wasmer Anda.</p>");
 }
 
 $conn->set_charset("utf8mb4");
